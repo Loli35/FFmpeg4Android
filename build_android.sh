f1c0839b2508a9cb0e926e0d78fd88d5a5477842
Loli35 <60430921+Loli35@users.noreply.github.com>
2020-01-30 17:32:24 -0500

    Update build_Andriod -sh

--- build_android.sh
+++ build_android.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NDK=/home/dennis/android-ndk-r9d
+NDK=/home/loli35/android-ndk-r9d
 SYSROOT=$NDK/platforms/android-9/arch-arm/
 TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64
  
@@ -7,20 +7,18 @@ function build_one
 {
   ./configure \
   --prefix=$PREFIX \
-  --enable-shared \
-  --disable-static \
-  --disable-doc \
-  --disable-ffserver \
-  --enable-cross-compile \
+  --disable-shared \
+  --enable-static \
+  --enable-doc \
+  --enable-ffserver \
+  --disable-cross-compile \
   --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
   --target-os=linux \
   --arch=arm \
   --sysroot=$SYSROOT \
-  --extra-cflags="-Os -fpic $ADDI_CFLAGS" \
-  --extra-ldflags="$ADDI_LDFLAGS" \
-  $ADDITIONAL_CONFIGURE_FLAG
+  
+  
 }
 CPU=arm
 PREFIX=$(pwd)/android/$CPU
-ADDI_CFLAGS="-marm"
 build_one
